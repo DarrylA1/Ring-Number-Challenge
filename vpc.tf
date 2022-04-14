5e8740d47086a0ab84aed86105903fff50a6fbd7
morganbrow <morgan.brown@cloudreach.com>
2022-04-14 09:44:45 -0400

    Updating outputs and provider

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,7 @@ resource "aws_eip" "nat_eip" {
 # Actually creating the nat gateway
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public_a.id
   tags = {
     Name = "gw NAT"
   }
