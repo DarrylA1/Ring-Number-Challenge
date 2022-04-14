47814c506bf428fc41987031058c05ab0458cf29
Lauren Johnson <101127959+laurenjcloudreach@users.noreply.github.com>
2022-04-14 16:43:03 -0400

    Python script to increment number in file
    Please review when able

---
+++ Incremental Python Script.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+#open and read the file after the appending:
+f = open("/Users/laurenjohnson/Documents/talent_academy/python_training/count.txt", "r") # open file in read mode
+count = f.read() # read data 
+count = int(count) + 1
+
+f = open("/Users/laurenjohnson/Documents/talent_academy/python_training/count.txt", "w") # open file in read mode
+f.write(str(count)) # write count to file
+f.close() # close file
+
+
+
+# In[12]:
+
+
+type(count)
+
+
+# In[ ]:
+
+
+
+
