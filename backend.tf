fe79bc2ca0e83f38c4fd7f3eaabe8acca0863d38
morganbrow <morgan.brown@cloudreach.com>
2022-04-18 09:11:07 -0400

    removing backend file

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "morgan-talent-academy-686520628199-tfstates-morgan"
-    key            = "projects/ring_vpc/terraform.tfstates"
-    dynamodb_table = "terraform-lock"
-
-  }
-}
